0d2638dc8d9ca021bd9345509a201e2790b70ca6
AaronOrtega1 <aaron.ortega15@outlook.es>
2024-01-25 17:11:53 -0600

    changes in fish config for python and conda

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,11 @@
 ### Starship setting prompt
 starship init fish | source
 
+### No greeting
 set -g fish_greeting
+
+### Alias
 alias py='python3'
+
+### CONDA PATH
+set -x PATH $PATH /home/aarondev/anaconda3/bin/
