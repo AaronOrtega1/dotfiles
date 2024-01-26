bc1c709a751a45d717c2cc1ea8e632ff9f46fd2d
AaronOrtega1 <aaron.ortega15@outlook.es>
2024-01-25 19:08:09 -0600

    minor changes on settings.json and starship, fish conda init

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,3 +9,16 @@ alias py='python3'
 
 ### CONDA PATH
 set -x PATH $PATH /home/aarondev/anaconda3/bin/
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/aarondev/anaconda3/bin/conda
+    eval /home/aarondev/anaconda3/bin/conda "shell.fish" hook $argv | source
+else
+    if test -f "/home/aarondev/anaconda3/etc/fish/conf.d/conda.fish"
+        . "/home/aarondev/anaconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH /home/aarondev/anaconda3/bin $PATH
+    end
+end
+# <<< conda initialize <<<
