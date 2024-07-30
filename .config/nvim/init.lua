56c6ff93ad687a1f1aa9a601e27d5930460c710b
AaronOrtega1 <aaron.ortega15@outlook.es>
2024-07-30 12:21:48 -0600

    added obsidian plugin to nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,9 @@
 -- init.lua
 
 if vim.g.vscode then
-	-- Carga los keybindings de VSCode
-	dofile(vim.fn.stdpath('config') .. '/vscode/keybindings.lua')
+  -- Carga los keybindings de VSCode
+  dofile(vim.fn.stdpath("config") .. "/vscode/keybindings.lua")
 else
-	-- Carga la configuración de Neovim con lazy.nvim
-	require("config.lazy")
+  -- Carga la configuración de Neovim con lazy.nvim
+  require("config.lazy")
 end
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,9 +1,3 @@
 vim.o.guifont = "FiraCode Nerd Font"
 vim.opt.wrap = true
 vim.opt.linebreak = true
-
-vim.scriptencoding = "utf-8"
-vim.opt.encoding = "utf-8"
-vim.opt.fileencoding = "utf-8"
-
-vim.opt.wildignore:append({ "*/node_modules/*" })
