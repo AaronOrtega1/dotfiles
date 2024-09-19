90d2af0fc047eaeca744ab51e0272f3342d755d0
AaronOrtega1 <aaron.ortega15@outlook.es>
2024-09-19 01:29:12 -0600

    changes in nvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,5 @@
 vim.o.guifont = "FiraCode Nerd Font"
 vim.opt.wrap = true
 vim.opt.linebreak = true
+vim.opt.shortmess:append("I") -- Evita que se muestre el mensaje de "insert" al guardar
+vim.opt.shortmess:append("F") -- Suprime el mensaje de "file" al guardar
