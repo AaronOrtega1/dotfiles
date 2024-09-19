74fa0f317e645591a432afdd41153159ebf1364e
AaronOrtega1 <aaron.ortega15@outlook.es>
2024-09-18 19:43:15 -0600

    created new script for screenshoot and waybar button

--- .config/hypr/scripts/screenshots.sh
+++ .config/hypr/scripts/screenshots.sh
@@ -1,1 +1,22 @@
-grimblast --notify copysave area ~/Pictures/Screenshots/$(date +'%s_screenshot.png') 
+#!/bin/bash
+
+# Directorio donde se guardarán las capturas
+DIRECTORIO="$HOME/Pictures/Screenshots/"
+
+# Nombre de archivo con la fecha y hora actual
+NOMBRE_ARCHIVO="screenshots_$(date +%Y-%m-%d_%H-%M-%S).png"
+
+# Ruta completa de la captura
+RUTA_COMPLETA="$DIRECTORIO/$NOMBRE_ARCHIVO"
+
+# Crear el directorio si no existe
+mkdir -p "$DIRECTORIO"
+
+# Usar slurp para seleccionar el área y grim para tomar la captura
+grim -g "$(slurp)" "$RUTA_COMPLETA"
+
+# Copiar la imagen al portapapeles utilizando wl-copy
+cat "$RUTA_COMPLETA" | wl-copy
+
+# Mensaje opcional para confirmar que se guardó la captura
+echo "Captura de pantalla guardada en $RUTA_COMPLETA y copiada al portapapeles."
